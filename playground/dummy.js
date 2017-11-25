b9d9037e09c3b2f4ae916bed931e3b8e81d0fe74
Julian Steffen <james14@mail.de>
2017-11-25 23:08:41 +0100

    Postman Post and Save

---
+++ playground/dummy.js
@@ -0,0 +1,30 @@
+// const newTodo = new Todo({
+//   text: 'Cook dinner',
+// });
+
+// newTodo.save().then((doc) => {
+//   console.log('Saved todo', doc);
+// }, (e) => {
+//   console.log('Unable to save todo');
+// });
+
+
+// const otherTodo = new Todo({
+//   text: 'My Todo',
+// });
+
+// otherTodo.save().then((doc) => {
+//   console.log(JSON.stringify(doc), undefined, 2);
+// }, (e) => {
+//   console.log('Unable to save todo', e);
+// });
+
+// const newUser = new User({
+//   email: 'mail@de',
+// });
+
+// newUser.save().then((doc) => {
+//   console.log(JSON.stringify(doc), undefined, 2);
+// }, (e) => {
+//   console.log('Unable to save todo', e);
+// });
